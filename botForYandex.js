538699091be3d7bf2654fd32c3bf2402e2ecb515
Fenderock <fender.vik@gmail.com>
2021-03-22 08:58:36 +0300

    Create botForYandex.js
    Программа работает с сайтом yandex.ru

---
+++ botForYandex.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://yandex.ru/
+// @grant        none
+// ==/UserScript==
+
+let keywords = ["Как звучит флейта","Валторна","Тромбон","Кларнет","Фагот","Гобой","Саксофон"];
+let randomIndex = Math.floor(Math.random()*keywords.length);
+let keyword = keywords[randomIndex];
+let yandexInput = document.getElementsByName('text')[0];
+let btn = document.getElementsByClassName('button')[1];
+
+if(btn!=undefined){
+    let i = 0;
+    let timerId = setInterval(()=>{
+        yandexInput.value += keyword[i++];
+        if(i==keyword.length){
+            clearInterval(timerId);
+            btn.click();
+        }
+    },250);
+}
+
+    let links = document.links;
+    let nextYandexPage = true;
+    for(let i=0; i<links.length; i++){
+        let link = links[i];
+        link.removeAttribute('target')
+        if(link.href.indexOf("soundtimes.ru")!=-1){
+            nextYandexPage = false;
+            link.click(); // Кликаем по ссылке
+            break; // Прерываем цикл
+        }
+    }
+    if(nextYandexPage) document.getElementsByClassName("pager__item_kind_next")[0].click();
