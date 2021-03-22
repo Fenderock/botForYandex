2dbe848185bb78aa35a0549003d68b96e9fb35f1
Fenderock <fender.vik@gmail.com>
2021-03-22 10:09:43 +0300

    Update botForYandex.js

--- botForYandex.js
+++ botForYandex.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://yandex.ru/
+// @match        https://yandex.ru/*
 // @grant        none
 // ==/UserScript==
 
